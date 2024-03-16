115114d11ba02f4b98188be78fc2b535aa0cd5d1
VitalySukhinin <vitaliksuhinin99@gmail.com>
2024-03-16 15:42:51 -0400

    created users classes and role class

---
+++ EventManagementSystem/Manager.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventManagementSystem
+{
+    public class Manager : User
+    {
+        public List<Event> Events {  get; set; }
+    }
+}
---
+++ EventManagementSystem/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventManagementSystem
+{
+    public abstract class User
+    {
+        public string Username {  get; set; }
+        public string Password { get; set; }
+
+        public Role Role { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+
+        public string Email { get; set; }
+
+        public string Phone {  get; set; }
+
+        public bool Valid { get; set; }
+
+    }
+}
