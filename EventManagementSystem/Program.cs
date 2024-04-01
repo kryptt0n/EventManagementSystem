a3c47de50dc93508e169e48cf8b2d18d2396bcf0
VitalySukhinin <vitaliksuhinin99@gmail.com>
2024-04-01 03:21:36 -0400

    restored db credentials

--- EventManagementSystem/Program.cs
+++ EventManagementSystem/Program.cs
@@ -3,7 +3,7 @@ namespace EventManagementSystem
 {
     internal static class Program
     {
-        public static Database db = Database.GetInstance("3.88.21.228", "3306", "vitaly", "vitalysukhinin", "humber");
+        public static Database db = Database.GetInstance("3.88.21.228", "3306", "username", "password", "humber");
 
         /// <summary>
         ///  The main entry point for the application.
